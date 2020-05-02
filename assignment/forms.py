c85f5e1497c13d5c4ec4d48808606913d231873a
Anupam Krishna <anupamkris13262@gmail.com>
2020-05-02 23:08:42 +0530

    0.3

---
+++ assignment/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+
+class RegistrationForm(FlaskForm):
+	username = StringField('Username',validators=[DataRequired(),Length(min=3, max=20)])
+	password = PasswordField('Password',validators=[DataRequired()])
+	confirmpassword = PasswordField('Confirm Password',
+		validators=[DataRequired(), EqualTo('password')])
+	
