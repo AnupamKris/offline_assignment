3adaaed50f9f82afd4c23c0a008b7f721c9b7710
Anupam Krishna <anupamkris13262@gmail.com>
2020-05-04 22:16:42 +0530

    1.6

--- assignment/forms.py
+++ assignment/forms.py
@@ -1,12 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, DateField
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from assignment.models import User
 
 class RegistrationForm(FlaskForm):
 	username = StringField('Username',validators=[DataRequired(),Length(min=3, max=20)],render_kw={"placeholder": "Username"})
 	admission = StringField('Admission Number', validators=[DataRequired()], render_kw={'placeholder':'Admission Number'})
-	dob = DateField('DOB',validators=[DataRequired()],render_kw={'placeholder':'DOB'})
+	dob = DateField('DOB',format='%Y-%m-%d',validators=[DataRequired()],render_kw={'placeholder':'DOB'})
 	email = StringField('Email', validators=[DataRequired(), Email()],render_kw={'placeholder':'Email'})
 	password = PasswordField('Password',validators=[DataRequired()],render_kw={'placeholder':'Password'})
 	confirmpassword = PasswordField('Confirm Password',
