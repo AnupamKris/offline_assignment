dc28cbbbc6d75475459057cafd0cfcf8c018bf39
Anupam Krishna <anupamkris13262@gmail.com>
2020-07-07 13:30:58 +0530

    15.4

--- assignment/routes.py
+++ assignment/routes.py
@@ -885,16 +885,21 @@ def create_test():
                         pdffile.write(img2pdf.convert(dellist))
                     for j in dellist:
                         os.remove(j)
-                    createfolder(filename)
-                    link = uploadfile('QP.pdf', filename)
+                    createfolder('onlinetest/'+filename)
+                    link = uploadfile('QP.pdf', 'onlinetest/'+filename)
                     break
                 else:
                     flash("You must choose atleast one type of file", 'fail2')
                     return redirect(url_for('create_test'))
             testsheet = client.open('onlinetests')
             worksheet = testsheet.worksheet('testsheet')
-            datalist = [current_user.email, request.form.get('class'), filename, request.form.get('qpnos'), request.form.get(
-                'testtimeduration'), request.form.get('teststartdate') + request.form.get('teststarttime'), request.form.get('testenddate') + request.form.get('testendtime'), link, '[]']
+            datalist = [current_user.email,
+                        request.form.get('class'),
+                        filename, request.form.get('qpnos'),
+                        request.form.get('testtimeduration'),
+                        request.form.get('teststartdate') +request.form.get('teststarttime'),
+                        request.form.get('testenddate') +request.form.get('testendtime'),
+                        link, '[]']
             worksheet.insert_row(datalist, 2)
             flash('Assignment created Successfully!', 'success2')
             os.remove('QP.pdf')
